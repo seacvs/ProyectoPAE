fd7daa8913e1c0176636ece6f92115be0c9d8cba
Sebastián Aceves <is702810@iteso.mx>
2018-11-08 09:32:07 -0600

    Con transiciones

--- ameyalli.java
+++ ameyalli.java
@@ -3,32 +3,62 @@ package proyecto;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ameyalli extends Application {
-	BorderPane root;
-	public static void main(String[] args) {
-		launch(args);
+	private static ameyalli instancia;
 	
+	
+	private Scene scene; 
+	private Stage primaryStage = new Stage();
+	private BorderPane pane_root;
+	
+	
+	public static ameyalli getInstance() {
+		if(instancia==null)
+			instancia = new ameyalli();
+		return instancia;
 	}
 	
+	private void setRootPane(BorderPane bp) {
+		pane_root = bp;
+	}
+	
+	public void setCenter(Node node) {
+		pane_root.setCenter(node);
+	}
+	
+	@Override
+	public void init() throws Exception{
+		super.init();
+		ameyalli.instancia = this;
+	}
+	
+	public void setScene(Pane node) {
+		scene = new Scene(node);
+		primaryStage.setScene(scene);
+	}
+		
 		@Override
 		public void start(Stage primaryStage) throws Exception {
-		
-			root = FXMLLoader.load(getClass().getResource("menu.fxml"));		// menu inicial con 4 tabs
-		
-			//VBox listado = FXMLLoader.load(getClass().getResource("nuevoCliente.fxml"));
-			//root.setCenter((Parent)(listado));
-			Scene scene = new Scene(root);			
-			scene.getStylesheets().add("proyecto/styles.css");
+			//pane_root = FXMLLoader.load(getClass().getResource("menu.fxml"));		// menu inicial con 4 tabs
+			setRootPane(FXMLLoader.load(getClass().getResource("menu.fxml")));
+			HBox listado = FXMLLoader.load(getClass().getResource("listado.fxml"));
+			pane_root.setCenter(listado);
 			
+			setScene(pane_root);
+			scene.getStylesheets().add("proyecto/styles.css");
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Rocódromo Ameyalli");
 			primaryStage.show();
@@ -36,16 +66,20 @@ public class ameyalli extends Application {
 	
 		public void gymLaunch() {
 			HBox listado = null;
+			
 			try {
 				listado = FXMLLoader.load(getClass().getResource("listado.fxml"));
-				root.setCenter((Parent)(listado));
-				Scene scene = new Scene(root);			
+				pane_root.setCenter((Parent)(listado));
+				Scene scene = new Scene(pane_root);			
 				scene.getStylesheets().add("proyecto/styles.css");
 				
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}		// menu inicial con 4 tabs
+		}
+		
+		public static void main(String[] args) {
+			launch(args);
 		}
 		
 	}
--- controllerMenu.java
+++ controllerMenu.java
@@ -1,26 +1,81 @@
 package proyecto;
 
+import java.awt.ScrollPane;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class controllerMenu extends ameyalli implements Initializable {
+public class controllerMenu implements Initializable {
+	
+	
+	
+	
+	public controllerMenu() {
+		super();
+	}
 	@FXML private Button gymBtn;
 
-	
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
-		
 	}
 	
 	@FXML 
 	private void gymInit(){
-		super.gymLaunch();
+		HBox listado = null;
+		try {
+			listado = FXMLLoader.load(getClass().getResource("listado.fxml"));
+			ameyalli.getInstance().setCenter(listado);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	@FXML 
+	private void tiendaInit(){
+		VBox ventas = null;
+		try {
+			ventas = FXMLLoader.load(getClass().getResource("puntoVenta.fxml"));
+			ameyalli.getInstance().setCenter(ventas);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	@FXML 
+	private void estadisticasInit(){
+		Node estadisticas = null;
+		try {
+			estadisticas = FXMLLoader.load(getClass().getResource("estadisticas.fxml"));
+			ameyalli.getInstance().setCenter(estadisticas);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	@FXML 
+	private void ajustesInit(){
+		HBox listado = null;
+		try {
+			listado = FXMLLoader.load(getClass().getResource("listado.fxml"));
+			ameyalli.getInstance().setCenter(listado);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
 	}
 	
 	
